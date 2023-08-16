16950c5ffc689aed4577291722ae2adc3ffaeb05
Md.Samsul Arefin <samsularefin532@gmail.com>
2023-08-16 17:57:40 +0600

    bank transition corrected

--- withdraw.js
+++ withdraw.js
@@ -3,14 +3,22 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
 
         const pwithdraw = parseFloat(document.getElementById('with-total').innerText);
         const nwithdraw = parseFloat(document.getElementById('withd').value);
-        const newtotal = pwithdraw - nwithdraw;
-        document.getElementById('with-total').innerText = newtotal;
-        document.getElementById('withd').value = '';
         const total_balance = parseFloat(document.getElementById('total-balance').innerText);
-        document.getElementById('total-balance').innerText = total_balance - nwithdraw;
+        if (total_balance >= nwithdraw) {
+            document.getElementById('total-balance').innerText = total_balance - nwithdraw;
+            const newtotal = pwithdraw - nwithdraw;
+            document.getElementById('with-total').innerText = newtotal;
+            document.getElementById('withd').value = '';
+        }
+        else {
+            alert('you do not have enough balance')
+            document.getElementById('withd').value = '';
+        }
+
     }
     else {
         alert('you do not have enough balance')
+        document.getElementById('withd').value = '';
     }
 
 })
